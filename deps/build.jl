816d402a66e7d01f43ee283536d99d7b0af555aa
eford <eford@users.noreply.github.com>
2025-08-22 21:05:40 +0000

    Deploying to gh-pages from @ PsuAstro528/lab3@a163be871b0ad95c701716cbd7a25c960ab1ad76 🚀

---
+++ deps/build.jl
@@ -0,0 +1,14 @@
+#println("Making sure Pluto is installed")
+import Pkg; 
+#Pkg.add(name="Pluto", version="0.20.7");
+import Pluto; 
+
+println("Installing packages for ex1.jl")
+Pluto.activate_notebook_environment("../ex1.jl"); 
+Pkg.instantiate(); 
+
+println("Installing packages for ex2.jl")
+Pluto.activate_notebook_environment("../ex2.jl"); 
+Pkg.instantiate(); 
+
+
